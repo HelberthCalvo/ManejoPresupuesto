dea1907d1b467e0d3464965e91c1114303a0f69e
Helberth Calvo <hcalvo4@hotmail.com>
2022-03-21 17:49:48 -0600

    CRUD de tipos cuentas y formilario para cuentas

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using ManejoPresupuesto.Servicios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
+builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
+builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
